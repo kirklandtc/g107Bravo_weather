df4b57bfcd5e4094dd96d794dfa29b5092b4cc4c
Thomas Kirkland <thomasckirkland@gmail.com>
2019-07-16 12:28:19 -0600

    adding API

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 // console.log("Wake Up!")
 // alert("Hello!!")
+window.addEventListener('load', ()=> {
+  let long;
+  let lat;
+
+  if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(position => {
+        long = position.coords.longitude;
+        lat = position.coods.latitude;
+
+        const api = `https://api.darksky.net/forecast/c9c4d3149a53a2d22839cf23c9022d7e/${lat},${long}`;
+
+      fetch(api)
+       .then(data => {
+         return data.json();
+       })
+       .then(data => {
+           });
+    });
+  }
+});
