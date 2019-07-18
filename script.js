264118b0d38b9efd1c4798fde4cfd97e2f524e22
Thomas Kirkland <thomasckirkland@gmail.com>
2019-07-18 11:39:30 -0600

    js changes

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-console.log("Wake Up!")
-alert("Hello!!")
+// console.log("Wake Up!")
+// alert("Hello!!")
 window.addEventListener('load', ()=> {
   let long;
   let lat;
+  let temperatureDescription = document.querySelector('.temperature-description');
+  let locationTimezone = document.querySelector('location-timezone');
 
-  if(navigator.geolocation){
+  if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(position => {
         long = position.coords.longitude;
         lat = position.coods.latitude;
 
-        const proxy ='https://cors-anywhere.herokuapp.com/';
-        const api = `${proxy}https://api.darksky.net/forecast/c9c4d3149a53a2d22839cf23c9022d7e/${lat},${long}`;
+        // const proxy ='https://cors-anywhere.herokuapp.com/';
+        const api = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/c9c4d3149a53a2d22839cf23c9022d7e/${lat},${long}`;
 
       fetch(api)
        .then(response => {
@@ -18,8 +20,11 @@ window.addEventListener('load', ()=> {
        })
        .then(data => {
          console.log(data);
+         const { temperature, summary } = data.currently;
+//Set DOM elements from the api
+         temperatureDegree.textContent = temperature;
 
         });
     });
-  }
+   }
 });
