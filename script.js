77ae72bb95e13d9c38c3906072e3ecf856b887f4
Thomas Kirkland <thomasckirkland@gmail.com>
2019-07-23 11:17:48 -0600

    added JS content

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 // console.log("Wake Up!")
 // alert("Hello!!")
 window.addEventListener('load', ()=> {
-  let long;
   let lat;
+  let long;
   let temperatureDescription = document.querySelector('.temperature-description');
+
+
+  let temperatureDegree = document.querySelector('.temperatureDegree');
   let locationTimezone = document.querySelector('location-timezone');
+  let temperatureSection = document.querySelector('temperature-section');
+  const temperatureSpan = document.querySelector('temperature span');
 
   if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(position => {
-        long = position.coords.longitude;
         lat = position.coods.latitude;
+        long = position.coords.longitude;
 
-        // const proxy ='https://cors-anywhere.herokuapp.com/';
-        const api = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/c9c4d3149a53a2d22839cf23c9022d7e/${lat},${long}`;
 
-      fetch(api)
+        const proxy ='https://cors-anywhere.herokuapp.com/';
+        const api = `${proxy}https://api.darksky.net/forecast/c9c4d3149a53a2d22839cf23c9022d7e/${lat},${long}`;
+
+       fetch(api)
        .then(response => {
          return response.json();
        })
@@ -23,6 +29,8 @@ window.addEventListener('load', ()=> {
          const { temperature, summary } = data.currently;
 //Set DOM elements from the api
          temperatureDegree.textContent = temperature;
+         temperatureDescription.textContent = summary;
+         locationTimezone.textContent = data.timezone;
 
         });
     });
