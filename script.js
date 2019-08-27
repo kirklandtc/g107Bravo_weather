68fbd57b1fc671d752ec4d30a60043827842b001
Thomas Kirkland <thomasckirkland@gmail.com>
2019-08-27 10:11:39 -0600

    minor changes

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ window.addEventListener('load', ()=> {
   let temperatureDescription = document.querySelector('.temperature-description');
 
 
-  let temperatureDegree = document.querySelector('.temperatureDegree');
-  let locationTimezone = document.querySelector('.location-timezone');
-  let temperatureSection = document.querySelector('.temperature-section');
-  const temperatureSpan = document.querySelector('.temperature span');
+  let temperatureDegree = document.querySelector(".temperatureDegree");
+  let locationTimezone = document.querySelector(".location-timezone");
+  let temperatureSection = document.querySelector(".temperature-section");
+  const temperatureSpan = document.querySelector(".temperature span");
 
   if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(position => {
@@ -17,7 +17,7 @@ window.addEventListener('load', ()=> {
         long = position.coords.longitude;
 
 
-         const proxy ='https://cors-anywhere.herokuapp.com/';
+         const proxy ="https://cors-anywhere.herokuapp.com/";
          const api = `${proxy}https://api.darksky.net/forecast/f617d7102d6fcef56be732aa0e4edc2f/${lat},${long}`;
 
 
@@ -39,12 +39,17 @@ window.addEventListener('load', ()=> {
 // Set Icon
          setIcons(icon, document.querySelector('.icon'));
 
+//Celsius
+        let celsius = (temperature - 32) * (5 / 9);
+
 //Alter Temperature between Celsius/Farenheit
          temperatureSection.addEventListener('click',() =>{
            if(temperatureSpan.textContent === "F"){
              temperatureSpan.textContent = "C";
+             temperatureDegree.textContent = Math.floor();
            } else {
              temperatureSpan.textContent = "F";
+             temperatureDegree.textContent = temperature;
            }
 
         });
